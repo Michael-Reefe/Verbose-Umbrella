564f5488224196b6ab0b63fe39e1eed7e314dc28
Michael-Reefe <michael.reefe8@gmail.com>
2021-07-16 21:43:02 -0400

    hydrogen atom

--- umbrella/poly.py
+++ umbrella/poly.py
@@ -1,6 +1,7 @@
 from umbrella.autodiff import *
 import mpl_toolkits.mplot3d.axes3d as axes3d
 import matplotlib.pyplot as plt
+from scipy import ndimage
 
 
 def factorial(n):
@@ -39,7 +40,7 @@ def _autodifferentiableP(x, l):
     return x, s
 
 
-def legendreP(x, l, m=0):
+def legendre(x, l, m=0):
     """
     Calculate the associated Legendre function Pnm of order n, m at the value x.
 
@@ -52,11 +53,23 @@ def legendreP(x, l, m=0):
         raise ValueError("abs(m) must be smaller than n")
     if m < 0:
         m = np.abs(m)
-        return (-1) ** m * factorial(l - m) / factorial(l + m) * legendreP(x, l, m)
+        return (-1) ** m * factorial(l - m) / factorial(l + m) * legendre(x, l, m)
     return (-1)**m * 2**l * (1 - x**2)**(m / 2) * sum([factorial(k)/factorial(k - m) * x**(k - m) * choose(l, k) * choose((l + k - 1) / 2, l) for k in range(m, l + 1)])
 
 
-def plot_legendre(m=0, l=None):
+def hermite(x, n):
+    return factorial(n) * sum([(-1)**m/(factorial(m)*factorial(n-2*m)) * (2*x)**(n-2*m) for m in range(n//2+1)])
+
+
+def _L(x, q):
+    return sum([(-1)**k/factorial(k) * choose(q, k) * x**k for k in range(q+1)])
+
+
+def laguerre(x, q, p=0):
+    return sum([(-1)**m * factorial(q+p)/(factorial(q-m)*factorial(p+m)*factorial(m)) * x**m for m in range(q+1)])
+
+
+def plot_legendre(m=0, l=None, *args, **kwargs):
     assert type(m) is int
     if not l:
         l = np.arange(np.abs(m), np.abs(m)+5, 1)
@@ -64,15 +77,15 @@ def plot_legendre(m=0, l=None):
     fig, ax = plt.subplots()
     y = []
     for li in l:
-        yi = [legendreP(xi, li, m) for xi in x]
+        yi = legendre(x, l, m)
         y.append(yi)
-        ax.plot(x, yi, label='$P_{%d}^{%d}$' % (li, m))
+        ax.plot(x, yi, label='$P_{%d}^{%d}$' % (li, m), *args, **kwargs)
     ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=len(l), fancybox=True)
     plt.show()
     return x, y
 
 
-def polar_plot_legendre(m=0, l=None):
+def polar_plot_legendre(m=0, l=None, *args, **kwargs):
     assert type(m) is int
     if not l:
         l = np.arange(np.abs(m), np.abs(m)+5, 1)
@@ -81,16 +94,53 @@ def polar_plot_legendre(m=0, l=None):
     ax.set_theta_offset(np.pi/2)
     r = []
     for li in l:
-        ri = np.abs(legendreP(np.cos(theta), li, m))
+        ri = np.abs(legendre(np.cos(theta), li, m))
         r.append(ri)
-        ax.plot(theta, ri, label='$P_{%d}^{%d}$' % (li, m))
+        ax.plot(theta, ri, label='$P_{%d}^{%d}$' % (li, m), *args, **kwargs)
     ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=len(l), fancybox=True)
     plt.show()
     return theta, r
 
 
+def plot_hermite(n=None, zoom=None, *args, **kwargs):
+    if not n:
+        n = np.arange(0, 5, 1)
+    x = np.linspace(-3, 3, 1000)
+    fig, ax = plt.subplots()
+    y = []
+    for ni in n:
+        yi = hermite(x, ni)
+        y.append(yi)
+        ax.plot(x, yi, label='$H_{%d}$' % ni, *args, **kwargs)
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=len(n), fancybox=True)
+    if zoom:
+        ax.set_xlim(*zoom[0])
+        ax.set_ylim(*zoom[1])
+    plt.show()
+    return x, y
+
+
+def plot_laguerre(p=0, q=None, zoom=None, *args, **kwargs):
+    assert type(p) is int
+    if not q:
+        q = np.arange(np.abs(p), np.abs(p)+5, 1)
+    x = np.linspace(-2, 10, 1000)
+    fig, ax = plt.subplots()
+    y = []
+    for qi in q:
+        yi = laguerre(x, qi, p)
+        y.append(yi)
+        ax.plot(x, yi, label='$L_{%d}^{%d}$' % (qi, p), *args, **kwargs)
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=len(q), fancybox=True)
+    if zoom:
+        ax.set_xlim(*zoom[0])
+        ax.set_ylim(*zoom[1])
+    plt.show()
+    return x, y
+
+
 def _harmonicY(theta, phi, l, m):
-    return np.sqrt((2*l+1)/(4*np.pi)*factorial(l-m)/factorial(l+m)) * legendreP(np.cos(theta), l, m) * np.exp(m*phi*complex('j'))
+    return np.sqrt((2*l+1)/(4*np.pi)*factorial(l-m)/factorial(l+m)) * legendre(np.cos(theta), l, m) * np.exp(m * phi * complex('j'))
 
 
 def harmonicY(theta, phi, l, m):
@@ -104,21 +154,21 @@ def harmonicY(theta, phi, l, m):
     :return: float, Ylm(theta, phi)
     """
     if m < 0:
-        return (-1)**m * np.sqrt(2) * np.sqrt((2*l+1)/(4*np.pi)*factorial(l-np.abs(m))/factorial(l+np.abs(m))) \
-               * legendreP(np.cos(theta), l, np.abs(m)) * np.sin(np.abs(m)*phi)
+        return (-1) ** m * np.sqrt(2) * np.sqrt((2*l+1)/(4*np.pi)*factorial(l-np.abs(m))/factorial(l+np.abs(m))) \
+               * legendre(np.cos(theta), l, np.abs(m)) * np.sin(np.abs(m) * phi)
     elif m == 0:
-        return np.sqrt((2*l+1)/(4*np.pi)) * legendreP(np.cos(theta), l, m)
+        return np.sqrt((2*l+1)/(4*np.pi)) * legendre(np.cos(theta), l, m)
     else:
-        return (-1)**m * np.sqrt(2) * np.sqrt((2*l+1)/(4*np.pi)*factorial(l-np.abs(m))/factorial(l+np.abs(m))) \
-               * legendreP(np.cos(theta), l, m) * np.cos(m * phi)
+        return (-1) ** m * np.sqrt(2) * np.sqrt((2*l+1)/(4*np.pi)*factorial(l-np.abs(m))/factorial(l+np.abs(m))) \
+               * legendre(np.cos(theta), l, m) * np.cos(m * phi)
 
 
-def plot_harmonics(m=0, l=None):
+def plot_harmonics(m=0, l=None, resolution=1, *args, **kwargs):
     assert type(m) is int
     if not l:
         l = 0
-    theta = np.linspace(0, np.pi, 100)
-    phi = np.linspace(-np.pi, np.pi, 100)
+    theta = np.linspace(0, np.pi, 100*resolution)
+    phi = np.linspace(-np.pi, np.pi, 100*resolution)
     THETA, PHI = np.meshgrid(theta, phi)
     R = np.abs(harmonicY(THETA, PHI, l, m))
     X = R * np.cos(PHI) * np.sin(THETA)
@@ -127,7 +177,7 @@ def plot_harmonics(m=0, l=None):
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1, projection='3d')
     ax.plot_surface(X, Y, Z, label='$Y_{%d}^{%d}$' % (l, m), rstride=1, cstride=1, cmap=plt.get_cmap('jet'),
-                    linewidth=0, antialiased=False, alpha=0.5)
+                    linewidth=0, antialiased=False, alpha=0.5, *args, **kwargs)
     ax.set_xlabel('$x$')
     ax.set_ylabel('$y$')
     ax.set_zlabel('$z$')
@@ -137,3 +187,34 @@ def plot_harmonics(m=0, l=None):
     ax.view_init(30, 45)
     plt.show()
     return X, Y, Z
+
+
+def hydrogen(r, theta, phi, n, l, m):
+    # Bohr radius
+    a = 0.529e-10
+    return np.sqrt((2/(n*a))**3 * factorial(n-l-1)/(2*n*factorial(n+l))) \
+        * np.exp(-r/(n*a)) * (2*r/(n*a))**l * laguerre(2*r/(n*a), n-l-1, 2*l+1) * _harmonicY(theta, phi, l, m)
+
+
+def hydrogen_cartesian(x, y, z, n, l, m):
+    r = np.sqrt(x**2 + y**2 + z**2)
+    theta = np.arctan2(np.sqrt(x**2+y**2), z)
+    phi = np.arctan2(y, x)
+    return hydrogen(r, theta, phi, n, l, m)
+
+
+def plot_hydrogen(n=1, l=0, m=0, resolution=1, *args, **kwargs):
+    # Bohr radius
+    a = 0.529e-10
+    assert type(n) is int
+    assert type(l) is int
+    assert type(m) is int
+    x = np.linspace(-30*a, 30*a, 100*resolution)
+    y = 0
+    z = np.linspace(-30*a, 30*a, 100*resolution)
+    X, Z = np.meshgrid(x, z)
+    density = np.abs(hydrogen_cartesian(X, y, Z, n, l, m))**2
+    fig, ax = plt.subplots()
+    ax.pcolor(X/a, Z/a, density, cmap='gnuplot', shading='nearest')
+    plt.show()
+    return X, y, Z, density
