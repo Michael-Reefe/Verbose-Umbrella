20d66595c3ba6abc822595ef4eb3fa08503f06b8
Michael-Reefe <michael.reefe8@gmail.com>
2021-07-20 16:42:46 -0400

    n

--- verbose_umbrella.py
+++ verbose_umbrella.py
@@ -57,6 +57,7 @@ def hydrogen_frame():
     l = st.number_input('l', int(0), int(n-1), int(0), int(1), "%d")
     m = st.number_input('m', int(-l), int(l), int(0), int(1), "%d")
     d = st.slider('Density cutoff (nm^-3)', float(0.), float(5.), float(0.25), float(0.001), "%.3f")
+    print(n, l, m, d)
     type = st.selectbox("Plot Type", ["2D", "3D"], 1)
     fig = build_hydrogen(n, l, m, d, type)
     if type == "3D":
